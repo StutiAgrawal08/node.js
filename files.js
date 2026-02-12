dcb64f5beb4352a85f10f38b4b7d1f851134080b
stuti agrawal <agrawalstuti59@gmail.com>
2026-02-12 15:14:16 +0530

    updated files.js

---
+++ files.js
@@ -0,0 +1,31 @@
+const a = ()=>{
+     setTimeout(()=>{
+        console.log("async function 1 : ")
+     }, 0);
+        // setTimeout(()=>{
+        // console.log("async function 2 :")
+        // }, 0);
+     b();
+     console.log("a");
+};
+
+const b = ()=>{
+     setTimeout(()=>{
+        console.log("async function 2 :")
+     }, 0);
+     console.log("b");
+     Promise.resolve().then(()=>{
+         console.log("asyn function 3 : ");
+     });
+     Promise.resolve().then(()=>{
+         console.log("asyn function 4 : ");
+     });
+
+    //  c();
+};
+
+// const c = ()=>{
+//      console.log("c");
+// };
+
+a();
